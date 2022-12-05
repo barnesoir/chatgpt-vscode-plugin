05b85e64e5cbd4c3d7656d53f720e2f052d44916
Jay Barnes <jxnnie@duck.com>
2022-12-05 01:39:32 +0000

    I've created the project, how do I add a Webview on the vscode command "Ask GPT" && How do I add this to a VSCode extension?

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,38 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	// Register the command handler
+	vscode.commands.registerCommand('chatgpt-vscode-plugin.askGPT', () => {
+		// Create the Webview panel
+		const panel = vscode.window.createWebviewPanel(
+			'webview', // Identifies the type of the webview. Used internally
+			'Webview', // Title of the panel displayed to the user
+			vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+			{
+				enableScripts: true, // Allow scripts in the webview
+			}
+		);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "chatgpt-vscode-plugin" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('chatgpt-vscode-plugin.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ChatGPT VSCode Plugin!');
+		// Set the HTML content of the Webview
+		panel.webview.html = `
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src https:; script-src 'unsafe-inline'; style-src vscode-resource: 'unsafe-inline';">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>My Webview</title>
+        </head>
+        <body>
+            <h1>Hello from the Webview!</h1>
+        </body>
+        </html>
+    `;
 	});
 
-	context.subscriptions.push(disposable);
+
+
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
