64cf06ce4bbf67f04fbb08517883585a3e4274e4
Jay Barnes <jxnnie@duck.com>
2022-12-07 00:21:54 +0000

    Add WebView

---
+++ src/chatgpt-view-provider.ts
@@ -0,0 +1,135 @@
+import { ChatGPTAPI } from 'chatgpt';
+import * as vscode from 'vscode';
+
+export default class ChatGptViewProvider implements vscode.WebviewViewProvider {
+    private webView?: vscode.WebviewView;
+    private chatGptApi?: ChatGPTAPI;
+    private sessionToken?: string;
+    private message?: any;
+
+    constructor(private context: vscode.ExtensionContext) {
+    }
+
+    public resolveWebviewView(
+        webviewView: vscode.WebviewView,
+        _context: vscode.WebviewViewResolveContext,
+        _token: vscode.CancellationToken,
+    ) {
+        this.webView = webviewView;
+        webviewView.webview.options = {
+            enableScripts: true,
+            localResourceRoots: [this.context.extensionUri]
+        };
+
+        webviewView.webview.html = this.getHtml(webviewView.webview);
+
+        webviewView.webview.onDidReceiveMessage(data => {
+            if (data.type === 'askChatGPT') {
+                this.sendApiRequest(data.value);
+            }
+        });
+
+        if (this.message !== null) {
+            this.sendMessage(this.message);
+            this.message = null;
+        }
+    }
+
+    public async setUpSessionToken() {
+        const sessionTokenName = 'chatgpt-session-token';
+        this.sessionToken = await this.context.globalState.get(sessionTokenName) as string;
+
+        if (!this.sessionToken) {
+            const userSessionToken = await vscode.window.showInputBox({ prompt: "Please enter your token (__Secure-next-auth.session-token), this can be retrieved using the guide on the README " })
+            this.sessionToken = userSessionToken!;
+            this.context.globalState.update(sessionTokenName, this.sessionToken);
+        }
+    }
+
+    public async sendApiRequest(prompt: string, code?: string) {
+        // Initialize session token and ChatGPT API if not done already
+        if (!this.sessionToken) {
+            await this.setUpSessionToken();
+        }
+        if (!this.chatGptApi) {
+            try {
+                this.chatGptApi = new ChatGPTAPI({ sessionToken: this.sessionToken as string });
+            } catch (error: any) {
+                vscode.window.showErrorMessage("Failed to connect to ChatGPT", error?.message);
+                return;
+            }
+        }
+
+        // Create question by adding prompt prefix to code, if provided
+        const question = (code) ? `${prompt}: ${code}` : prompt;
+
+        if (!this.webView) {
+            await vscode.commands.executeCommand('chatgpt-vscode-plugin.view.focus');
+        } else {
+            this.webView?.show?.(true);
+        }
+
+        // Send question to ChatGPT and show response
+        this.sendMessage({ type: 'addQuestion', value: prompt, code });
+        try {
+            await this.chatGptApi?.ensureAuth();
+            const response = await this.chatGptApi?.sendMessage(question);
+            this.sendMessage({ type: 'addResponse', value: response });
+        } catch (error: any) {
+            await vscode.window.showErrorMessage("Error sending request to ChatGPT", error?.message);
+            return;
+        }
+    }
+
+    public sendMessage(message: any) {
+        if (this.webView) {
+            this.webView?.webview.postMessage(message);
+        } else {
+            this.message = message;
+        }
+    }
+
+    private getHtml(webview: vscode.Webview) {
+
+        const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, 'media', 'main.js'));
+        const stylesMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, 'media', 'main.css'));
+
+        return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<link href="${stylesMainUri}" rel="stylesheet">
+				<script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
+				<script src="https://cdn.tailwindcss.com"></script>
+			</head>
+			<body class="overflow-hidden">
+				<div class="flex flex-col h-screen">
+					<div class="flex-1 overflow-y-auto" id="qa-list"></div>
+					<div id="in-progress" class="p-4 flex items-center hidden">
+                        <div style="text-align: center;">
+                            <div>Please wait while we handle your request ❤️</div>
+                            <img src="https://i.giphy.com/media/ule4vhcY1xEKQ/giphy.webp" alt="loading gif">
+                            <div>Please note, ChatGPT facing scaling issues which will impact this extension</div>
+                        </div>
+					</div>
+					<div class="p-4 flex items-center">
+						<div class="flex-1">
+							<textarea
+								type="text"
+								rows="2"
+								class="border p-2 w-full"
+								id="question-input"
+								placeholder="Ask a question..."
+							></textarea>
+						</div>
+						<button style="background: var(--vscode-button-background)" id="ask-button" class="p-2 ml-5">
+							Ask
+						</button>
+					</div>
+				</div>
+				<script src="${scriptUri}"></script>
+			</body>
+			</html>`;
+    }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,96 +1,47 @@
 import * as vscode from 'vscode';
-import { ChatGPTAPI } from 'chatgpt';
-
-const sessionTokenName = 'sessionToken';
+import ChatGptViewProvider from './chatgpt-view-provider';
 
 export async function activate(context: vscode.ExtensionContext) {
-	context.globalState.setKeysForSync([sessionTokenName]);
-
-	if (!context.globalState.get(sessionTokenName)) {
-		await setUpGPT();
-	}
-
-	const chatApi = new ChatGPTAPI({ sessionToken: context.globalState.get(sessionTokenName) as string });
+	const chatViewProvider = new ChatGptViewProvider(context);
 
 	context.subscriptions.push(
-		vscode.commands.registerCommand('chatgpt-vscode-plugin.setUpGPT', setUpGPT),
 		vscode.commands.registerCommand('chatgpt-vscode-plugin.askGPT', askChatGPT),
 		vscode.commands.registerCommand('chatgpt-vscode-plugin.whyBroken', askGPTWhyBroken),
 		vscode.commands.registerCommand('chatgpt-vscode-plugin.explainPls', askGPTToExplain),
 		vscode.commands.registerCommand('chatgpt-vscode-plugin.refactor', askGPTToRefactor),
-		vscode.commands.registerCommand('chatgpt-vscode-plugin.clearLogs', clearChatGPTLogs),
+		vscode.commands.registerCommand('chatgpt-vscode-plugin.addTests', askGPTToAddTests),
+		vscode.commands.registerCommand('chatgpt-vscode-plugin.resetToken', resetToken),
+		vscode.window.registerWebviewViewProvider("chatgpt-vscode-plugin.view", chatViewProvider, {
+			webviewOptions: { retainContextWhenHidden: true }
+		})
 	);
-
-	async function clearChatGPTLogs() {
-		await vscode.workspace.fs.writeFile(vscode.Uri.file('/tmp/ChatGPT'), Buffer.from(''));
-		vscode.window.showInformationMessage('ChatGPT logs cleared');
-	}
-
-	async function setUpGPT() {
-		let res = await vscode.window.showInputBox({ prompt: 'Please enter your access token, this can be retrieved using the guide on the README' });
-		context.globalState.update(sessionTokenName, res);
-	}
 
 	async function askGPTToExplain() { await askChatGPT('Can you explain what this code does?'); }
 	async function askGPTWhyBroken() { await askChatGPT('Why is this code broken?'); }
 	async function askGPTToRefactor() { await askChatGPT('Can you refactor this code and explain what\'s changed?'); }
+	async function askGPTToAddTests() { await askChatGPT('Can you add tests for this code?'); }
 
-	async function askChatGPT(queryOverride?: string) {
-		let stateSessionToken: string | undefined = context.globalState.get(sessionTokenName);
-		if (!stateSessionToken) {
-			return vscode.window.showErrorMessage('You need to set up your access token first, run ChatGPT: Login');
+	async function resetToken() {
+		await context.globalState.update('chatgpt-session-token', null);
+		await vscode.window.showInformationMessage("Token reset, you'll be prompted for it next to you next ask ChatGPT a question.");
+	}
+
+	async function askChatGPT(userInput?: string) {
+		if (!userInput) {
+			userInput = await vscode.window.showInputBox({ prompt: "Ask ChatGPT a question" }) || "";
 		}
 
-		if (!queryOverride) {
-			const query = await vscode.window.showInputBox({ prompt: 'Enter your query' });
-			if (!query) {
-				return;
-			}
-			queryChatGPT(query, chatApi);
-		} else {
-			queryChatGPT(queryOverride, chatApi);
+		let editor = vscode.window.activeTextEditor;
+
+		if (editor) {
+			const selectedCode = editor.document.getText(vscode.window.activeTextEditor?.selection);
+			const entireFileContents = editor.document.getText();
+
+			const code = selectedCode
+				? selectedCode
+				: `This is the ${editor.document.languageId} file I'm working on: \n\n${entireFileContents}`;
+
+			chatViewProvider.sendApiRequest(userInput, code);
 		}
 	}
 }
-
-const queryChatGPT = async (userInput: string, api: ChatGPTAPI) => {
-	await textToMarkdownPreview(spinner, "Thinking...", false);
-	const languageId = vscode.window.activeTextEditor?.document.languageId;
-	const selectedCode = vscode.window.activeTextEditor?.document.getText(vscode.window.activeTextEditor?.selection);
-	const entireFileContents = vscode.window.activeTextEditor?.document.getText();
-
-	const query = selectedCode
-		? selectedCode + "\n\n" + userInput
-		: `This is the ${languageId} file I'm working on: \n\n` + entireFileContents + "\n\n" + userInput;
-
-	console.log('Querying ChatGPT...', { query });
-
-	const response = await api.sendMessage(query);
-	console.log('ChatGPT response:', { response });
-	await textToMarkdownPreview(response, userInput);
-};
-
-const textToMarkdownPreview = async (textResponse: string, query: string, shouldAppend: boolean = true) => {
-	if (shouldAppend) {
-		const chatLog = vscode.Uri.file('/tmp/ChatGPT');
-		let tempFileContents: string = '';
-
-		try {
-			tempFileContents = (await vscode.workspace.fs.readFile(chatLog)).toString();
-		} catch (EntryNotFound) { }
-
-		await vscode.workspace.fs.writeFile(chatLog, Buffer.from(`${tempFileContents}\n\n# ${query}\n\n${textResponse}`));
-		await vscode.commands.executeCommand('markdown.showPreview', chatLog);
-	} else {
-		const tempFile = vscode.Uri.file('/tmp/ChatGPT-temp');
-		await vscode.workspace.fs.writeFile(tempFile, Buffer.from(textResponse));
-		await vscode.commands.executeCommand('markdown.showPreview', tempFile);
-	}
-};
-
-const spinner = ` <body>
-<h1 style="text-align: center; font-family: sans-serif;">Please wait while we fetch your request</h1>
-<div style="text-align: center;">
-  <img src="https://i.giphy.com/media/ule4vhcY1xEKQ/giphy.webp" alt="loading gif">
-</div>
-</body>`;
